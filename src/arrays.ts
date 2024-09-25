dcfe942d49ff11a8793ca9a069a38d974686ca47
LAPTOP-AL8JKVBA\saura <saurav.pokhrel@maine.edu>
2024-09-24 22:12:49 -0400

    function 5

--- src/arrays.ts
+++ src/arrays.ts
@@ -57,7 +57,10 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    return 0;
+    const fourWordsArray = words.filter(
+        (word: string): boolean => word.length <= 4,
+    );
+    return fourWordsArray.length;
 }
 
 /**
